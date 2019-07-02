982694f644aaf3d6793db4039c4b69dfdb1a155e
Adrian Garcia <adrigar90@gmail.com>
2019-07-02 21:42:07 +0100

    Redis integration

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,33 +2,47 @@
 const express = require('express');
 const userRouter = express.Router();
 
-// users in memory
-let loggedUsers = []
+const ChatRedis = require('../redis')
+
+// We just want to store the logged users (username has to be unique)
+// so we always use the same key to adapt it to our Redis implementation
+const key = 'unique'
+
 
 // Login
 userRouter.post('/login', (req, res) => {
-    const newuser = req.body
-    if(!newuser.username) return res.send({code: 400, message: 'Data is required'})
+    const newUser = req.body
+    if (!newUser.username) return res.send({ code: 400, message: 'Data is required' })
 
-    console.log(`Login user ${newuser.username}`)
+    console.log(`Login user ${newUser.username}`)
 
-    const isLoggedIn = loggedUsers.some( user => user.username === newuser.username)
-    if(isLoggedIn) return res.send({code: 401, message: 'Username already exists'})
-    loggedUsers.push(req.body)
-    res.send({code: 200, message: 'Logged in succesfully'})
+    ChatRedis.getUser(newUser.username, key)
+        .then(user => {
+            if (user === null) {
+                ChatRedis.addUser(newUser.username, key, newUser)
+                console.log(`User ${newUser.username} logged`)
+                return res.send({ code: 200, message: 'Logged in succesfully' })
+            }
+
+            console.log(`User ${newUser.username} already exists`)
+            return res.send({ code: 401, message: 'Username already exists' })
+        })
 })
 
 // Logout
 userRouter.post('/logout', (req, res) => {
-    const logoutUser = req.body
+    const user = req.body
 
-    const i = loggedUsers.findIndex( user => user.username === logoutUser.username)
-    if(i < 0) return res.send({code: 400, message: 'User not found'})
+    console.log(`Logout user ${user.username}`)
 
-    console.log(`Logout user ${logoutUser.username}`)
-
-    loggedUsers.splice(i, 1)
-    res.send({code: 200, message: 'Logged in succesfully'})
+    ChatRedis.delUser(user.username, key)
+        .then( data => {
+            if(data === null) {
+                return res.send({ code: 400, message: 'User not found' })
+            }
+                
+            return res.send({ code: 200, message: 'Logged in succesfully' })
+        })
 })
 
 
